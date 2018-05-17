aff5cf0d3a393b6a3406b22c3a42cac37e1258a9
Michał Urbaniak <blali733@gmail.com>
2018-05-17 17:06:43 +0200

    Updated time recording

--- Assets/Scripts/TargetSpawner.cs
+++ Assets/Scripts/TargetSpawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SharedTypes;
@@ -10,6 +11,8 @@ public class TargetSpawner : MonoBehaviour
     private int _itemCounter = 0;
     private Vector2Int _range;
     private ColorMode _colorMode;
+    private TestCase _testCase;
+    private TargetData _targetData;
 
 	// Use this for initialization
 	private void Start ()
@@ -19,29 +22,39 @@ public class TargetSpawner : MonoBehaviour
 
     public void Setup(TestCase myTestCase)
     {
-        _itemCounter = myTestCase.TargetsCount;
-        _range = myTestCase.MinTargetScale > myTestCase.MaxTargetScale ? new Vector2Int(myTestCase.MaxTargetScale, myTestCase.MinTargetScale) : new Vector2Int(myTestCase.MinTargetScale, myTestCase.MaxTargetScale);
-        _config.SetCameraProperties(myTestCase.DisplayMode);
-        _colorMode = myTestCase.Color;
+        _testCase = myTestCase;
+        _itemCounter = _testCase.TargetsCount;
+        _range = _testCase.MinTargetScale > _testCase.MaxTargetScale ? new Vector2Int(_testCase.MaxTargetScale, _testCase.MinTargetScale) : new Vector2Int(_testCase.MinTargetScale, _testCase.MaxTargetScale);
+        _config.SetCameraProperties(_testCase.DisplayMode);
+        _colorMode = _testCase.Color;
         GameObject target = Instantiate(TargetPrefab);
         target.transform.parent = this.transform;
         float size = _config.GetRandomSize(_range);
         Vector2 position = new Vector2(0,0);
         Color color = _config.GetColor(_colorMode);
-        SendMessageUpwards("TargetAcquired", new TargetData(color, position[0], position[1], size));
+        _targetData = new TargetData(color, position[0], position[1], size, DateTime.Now);
         target.GetComponent<TargetController>().Construct(size, position, color);
     }
 
-    public void Respawn()
+    public void Respawn(DateTime now)
     {
-        if (_itemCounter <= 0) return;
-        GameObject target = Instantiate(TargetPrefab);
-        target.transform.parent = this.transform;
-        float size = _config.GetRandomSize(_range);
-        Vector2 position = _config.GetRandomPosition();
-        Color color = _config.GetColor(_colorMode);
-        SendMessageUpwards("TargetAcquired", new TargetData(color, position[0], position[1], size));
-        target.GetComponent<TargetController>().Construct(size, position, color);
-        _itemCounter--;
+        if (_itemCounter <= 0)
+        {
+            _targetData.DestroyTime = now;
+            SendMessageUpwards("TargetAcquired", _targetData);
+        }
+        else
+        {
+            _targetData.DestroyTime = now;
+            SendMessageUpwards("TargetAcquired", _targetData);
+            GameObject target = Instantiate(TargetPrefab);
+            target.transform.parent = this.transform;
+            float size = _config.GetRandomSize(_range);
+            Vector2 position = _config.GetRandomPosition(_testCase.DistanceMode, _testCase.Radius, _targetData);
+            Color color = _config.GetColor(_colorMode);
+            _targetData = new TargetData(color, position[0], position[1], size, System.DateTime.Now);
+            target.GetComponent<TargetController>().Construct(size, position, color);
+            _itemCounter--;
+        }
     }
 }
